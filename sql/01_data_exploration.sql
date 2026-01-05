c21c6fb6add69e050f6f8ebde3ab2b8cfe9b41a6
Akshita Goswami <iamakshita29@gmail.com>
2026-01-05 19:21:22 +0530

    Add data exploration SQL queries

---
+++ sql/01_data_exploration.sql
@@ -0,0 +1,10 @@
+-- Data Exploration Queries
+
+-- Preview first 10 rows of the movies table
+SELECT *
+FROM movies
+LIMIT 10;
+
+-- Total number of movies
+SELECT COUNT(*) AS total_movies
+FROM movies;
